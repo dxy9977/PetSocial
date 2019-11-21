3084244aec7c057315821843ab3ab419453b7365
qinyu <371241240@qq.com>
2019-11-21 18:35:55 +0800

    test

--- app/src/main/java/com/example/petsocial/fragment/CustomerFragment.java
+++ app/src/main/java/com/example/petsocial/fragment/CustomerFragment.java
@@ -26,7 +26,8 @@ public class CustomerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_customer, container, false);
+        return inflate;
     }
 
 }
