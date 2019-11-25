3977233a4fef5725c5d7affbc58a22092bcb6331
qinyu <371241240@qq.com>
2019-11-25 20:29:22 +0800

    test qinyu

---
+++ app/src/main/java/com/example/petsocial/ui/AddFriend.java
@@ -0,0 +1,16 @@
+package com.example.petsocial.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.petsocial.R;
+
+public class AddFriend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+    }
+}
